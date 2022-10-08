42de799a5fa81ce6d5814a464226aaefcc2d1169
Arslan Ali <arslanali823@gmail.com>
2022-10-09 00:55:00 +0500

    auth-working

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 window._ = _;
 
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap';
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -11,6 +16,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
